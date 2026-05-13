555285c29e89177b1f7260207ebd4f07bb9d30c6
akujeeez <ajrupisan16@gmail.com>
2026-05-13 17:51:52 +0800

    LE3 complete

---
+++ source/repos/RupisanLE2/BlogDataLibrary/Data/ISqlData.cs
@@ -0,0 +1,19 @@
+﻿using BlogDataLibrary.Models;
+
+namespace BlogDataLibrary.Data
+{
+    public interface ISqlData
+    {
+        void AddPost(PostModel post);
+        UserModel Authenticate(string username, string password);
+        void CreatePost(PostModel post);
+        void CreateUser(UserModel user);
+        List<ListPostModel> GetAllPosts();
+        List<UserModel> GetAllUsers();
+        PostModel GetPost(int id);
+        UserModel GetUser(string userName, string password);
+        List<ListPostModel> ListPosts();
+        void Register(string username, string firstName, string lastName, string password);
+        ListPostModel ShowPostDetails(int id);
+    }
+}
--- source/repos/RupisanLE2/BlogDataLibrary/Data/SqlData.cs
+++ source/repos/RupisanLE2/BlogDataLibrary/Data/SqlData.cs
@@ -3,7 +3,7 @@ using BlogDataLibrary.Models;
 
 namespace BlogDataLibrary.Data
 {
-    public class SqlData
+    public class SqlData : ISqlData
     {
         private ISqlDataAccess _db;
         private const string connectionStringName = "SqlDb";
@@ -47,6 +47,40 @@ namespace BlogDataLibrary.Data
         {
             string sql = "INSERT INTO dbo.Posts (UserId, Title, Body, DateCreated) VALUES (@UserId, @Title, @Body, @DateCreated)";
             _db.SaveData(sql, post, connectionStringName, false);
+        }
+
+        public UserModel Authenticate(string username, string password)
+        {
+            UserModel result = _db.LoadData<UserModel, dynamic>(
+                    "dbo.spUsers_Authenticate",
+                     new { username, password },
+                     connectionStringName,
+                         true).FirstOrDefault();
+
+            return result;
+        }
+
+        public void Register(string username, string firstName, string lastName, string password)
+        {
+            _db.SaveData<dynamic>("dbo.spUsers_Register",
+                new { username, firstName, lastName, password },
+                connectionStringName,
+                true);
+        }
+        public void AddPost(PostModel post)
+        {
+            _db.SaveData("dbo.spPosts_Insert",
+                new { post.UserId, post.Title, post.Body, post.DateCreated },
+                connectionStringName,
+                true);
+        }
+        public List<ListPostModel> ListPosts()
+        {
+            return _db.LoadData<ListPostModel, dynamic>("dbo.spPosts_List", new { }, connectionStringName, true).ToList();
+        }
+        public ListPostModel ShowPostDetails(int id)
+        {
+            return _db.LoadData<ListPostModel, dynamic>("dbo.spPosts_Detail", new { id }, connectionStringName, true).FirstOrDefault();
         }
     }
 }
